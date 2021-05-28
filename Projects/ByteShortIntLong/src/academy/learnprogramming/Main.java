66659461f2fd7009bd7b25f8069a3d0244fd7c2f
Kreshnik Zeneli <kreshnik.zeneli@rks-gov.net>
2021-05-28 15:44:42 +0200

    1 Week of Java Exercises

---
+++ Projects/ByteShortIntLong/src/academy/learnprogramming/Main.java
@@ -0,0 +1,28 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
+        System.out.println("Busted MIN value = " + (myMinIntValue - 1));
+
+        int myMaxIntTest = 2147483647;
+
+
+
+//   This a code to test MAX_VALUE of INT data type
+
+//        int budgetTotal1 = 1680463568;
+//        int budgetTotal2 = 467020079 ;
+//        int budgetTotal = budgetTotal1 + budgetTotal2 ;
+//        System.out.println("Total Budget is = " + budgetTotal);
+
+    }
+}

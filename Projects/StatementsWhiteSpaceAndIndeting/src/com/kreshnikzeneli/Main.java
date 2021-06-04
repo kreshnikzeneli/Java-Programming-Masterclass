49639a6d907516cc39f97eba20097c4d5c77fe58
Kreshnik Zeneli <kreshnik.zeneli@rks-gov.net>
2021-06-04 15:32:19 +0200

    Exercises of date 04062021

---
+++ Projects/StatementsWhiteSpaceAndIndeting/src/com/kreshnikzeneli/Main.java
@@ -0,0 +1,23 @@
+package com.kreshnikzeneli;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable = 50;
+        if (myVariable == 50) {
+            System.out.println("Printed");
+        }
+
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test = " + myVariable);
+
+        System.out.println("This is" +
+                " another" +
+                " still more.");
+
+        int anotherVariable = 50;
+        myVariable--;
+        System.out.println("This is another one");
+    }
+}

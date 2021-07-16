90f43ae9095954af35c2efb7befab4250ad5fedb
Kreshnik Zeneli <kreshnik.zeneli@rks-gov.net>
2021-07-16 15:46:36 +0200

    Exercises of the date 16072021

---
+++ Projects/classesPart1/src/com/kreshnikzeneli/Car.java
@@ -0,0 +1,25 @@
+package com.kreshnikzeneli;
+
+public class Car {
+
+    private int doors;
+    private int weels;
+    private String model;
+    private String engine;
+    private String color;
+
+    public void setModel(String model) {
+        String validModel = model.toLowerCase();
+        if (validModel.equals("carrera") || validModel.equals("commodore")) {
+            this.model = model;
+        } else {
+            this.model = "Unknown";
+        }
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+
+}
